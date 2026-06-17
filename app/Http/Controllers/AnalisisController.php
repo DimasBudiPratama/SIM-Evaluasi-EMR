87beae1985960607bd2ec838a65e8fece6b5123e
dimasbudipratama <dimasbudipratama007.com>
2026-06-17 10:22:48 +0700

    Pesan commit pertama saya

--- app/Http/Controllers/AnalisisController.php
+++ app/Http/Controllers/AnalisisController.php
@@ -208,22 +208,6 @@ class AnalisisController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
